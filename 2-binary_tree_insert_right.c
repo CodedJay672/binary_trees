be2c9a2156d0f2179f6ac869b512f0d1ecb5fd25
codedjay672 <emmajoel672@gmail.com>
2022-12-15 23:45:22 +0000

    update binary file

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *new_right;
 
 	if (parent == NULL)
-		return NULL;
+		return (NULL);
 
 	new_right = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (new_right == NULL)

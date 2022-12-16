584a36baa157f9f1c22cdf7b97a701190e415c3f
codedjay672 <emmajoel672@gmail.com>
2022-12-16 22:39:15 +0000

    update untracked files

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect -> funct which checks if a tree is perfect
+ * @tree: pointer to the root of the tree
+ * Return: returns 1 if true else 0
+ *
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int l_sub, r_sub, count = 0;
+
+	if (tree == NULL)
+		return (0);
+
+}

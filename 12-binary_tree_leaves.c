1b7b1480a38c487ea41404c73f1020e38c6fa24c
codedjay672 <emmajoel672@gmail.com>
2022-12-16 09:10:02 +0000

    update 12-binary_tree_leaves.c files

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,7 +10,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->left && tree->right)
-		return (2);
-	return (1);
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (1);
+	return (binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right));
 }
